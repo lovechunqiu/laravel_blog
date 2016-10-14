ffe22c8311e7d8b944de10b5ec5ce806e762019c
lovechunqiu <lideqiang110119@126.com>
2016-10-14 17:20:11 +0800

    后台文章分页列表、分页

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Validator;
 class ArticleController extends CommonController
 {
     //get.admin/article  全部文章列表
-    public function index(){
-        echo '全部文章列表';
+    public function index()
+    {
+
+        $data = Article::orderBy('art_id', 'desc')->paginate(10);
+        return view('admin.article.index', compact('data'));
     }
 
     //post.admin/article/create 添加文章
